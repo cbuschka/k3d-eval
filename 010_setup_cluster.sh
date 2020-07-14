8b01255d0c3d664b07de34e1bfb1368cd781d2f9
conni <cbuschka@gmail.com>
2020-07-14 19:49:31 +0200

    Fixed status call.

--- 010_setup_cluster.sh
+++ 010_setup_cluster.sh
@@ -6,11 +6,9 @@ cd ${PROJECT_DIR}
 source ${PROJECT_DIR}/configrc
 
 echo "Creating cluster ${CLUSTER_NAME}..."
-./k3d create --api-port 6443 --publish 0.0.0.0:80:80 --publish 0.0.0.0:443:443 --name=${CLUSTER_NAME} --workers 2
+./k3d create --api-port 6443 --publish 0.0.0.0:80:80 --publish 0.0.0.0:443:443 --name=${CLUSTER_NAME} --workers ${WORKER_COUNT}
 
 echo "Starting cluster..."
 ./k3d start --name=${CLUSTER_NAME}
 
-./k3d list
-
-./012_show_status.sh
+./011_show_status.sh
